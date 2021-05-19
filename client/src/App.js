2d4ef7f5b537a717e4d0af4c3e30f4481f0c6c39
Andrej Oljaca <65299043+aoljaca@users.noreply.github.com>
2021-05-19 11:28:11 -0400

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,10 @@ import { Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 import Student from './components/showStudent/showStudent.js';
 import Create from './components/createStudent/createStudent.js';
 import './App.css';
-
-// test
+import useStyles from './styles';
 
 function App() {
+  const classes = useStyles();
   return (
     <div className="App">
       <Container maxWidth="lg">
---
+++ client/src/components/createStudent/createStudent.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+
+export default function BasicTextFields() {
+  const classes = useStyles();
+
+  return (
+    <>
+        <h2>Create Student</h2>
+        <form className={classes.root} noValidate autoComplete="off">
+            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            <Button variant="contained" color="primary">
+                Create
+            </Button>
+        </form>
+    </>
+  );
+}
